6693a4ad8ad6204ac83c33146dd58195a48adaf5
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-01-20 10:05:56 +0400

    fix

--- index.php
+++ index.php
@@ -2,12 +2,22 @@
 use infrajs\ans\Ans;
 use infrajs\path\Path;
 use infrajs\rest\Rest;
+use infrajs\template\Template;
 use akiyatkin\sitemap\Sitemap;
 
 
 return Rest::get( function () {
 	$data = Sitemap::data();
 	return Ans::ans($data);
+},'xml', function () {
+	$list = Sitemap::data();
+	$data = [];
+	$data['list'] = $list;
+	$data['host'] = $_SERVER['HTTP_HOST'];
+	$data['protocol'] = 'https';
+	$html = Template::parse('-sitemap/layout.tpl', $data, 'XML');
+	header('Content-type:application/xml; charset=utf-8');
+	return Ans::html($html);
 });
 
 
--- layout.tpl
+++ layout.tpl
@@ -5,3 +5,14 @@
 {::item}
 {heading:}<h2 class="mt-3">{~key}</h2>
 {item:}<a href="/{loc}">{title}</a>, 
+{XML:}<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+	{list::xmlgroup}
+</urlset>
+{xmlgroup:}{::xmlitem}
+{xmlitem:}<url>
+        <loc>{protocol}://{host}/{loc}</loc>
+        <lastmod>{lastmod}</lastmod>
+        <changefreq>{changefreq}</changefreq>
+        <priority>{priority}</priority>
+    </url>
